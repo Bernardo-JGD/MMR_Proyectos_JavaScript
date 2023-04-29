835a6f4dedda2e32bb52a1536dcf24ee52f75e26
Bernardo-JGD <guzman.diaz.jb@gmail.com>
2023-04-29 00:18:25 -0600

    Agregando startGame juego gato

--- 6_JuegoGato/main.js
+++ 6_JuegoGato/main.js
@@ -1,13 +1,41 @@
 const board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
 ]; // La matriz que representa al arreglo
 
 let turn = 0; // 0 = para el usuario  y 1 para la computadora
 //Seleccionando html del index
 const boardContainer = document.querySelector('#board');
 const playerDiv = document.querySelector('#player');
+
+startGame();
+
+function startGame(){
+    renderBoard();
+    turn = Math.random() <= 0.5 ? 0 : 1; //Cada vez que cargue la pagina puede variar quien empieza
+
+    renderCurrentPlayer();
+
+    if(turn === 0){
+        playerPlays();
+    }else{
+        PCPlays();
+    }
+}
+
+function playerPlays(){
+
+}
+
+function PCPlays(){
+    
+}
+
+function renderCurrentPlayer(){
+    //Cambia la etitqueta de quien esta en turno
+    playerDiv.textContent = `${turn === 0 ? 'Player Turn': 'PC Turn'}`;
+}
 
 function renderBoard(){
     const html = board.map((row) =>{//Itero filas
