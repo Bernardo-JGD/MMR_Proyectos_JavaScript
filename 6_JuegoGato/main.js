564ad4fec334838196560904cdc462ce9328854a
Bernardo-JGD <guzman.diaz.jb@gmail.com>
2023-05-11 18:33:36 -0600

    Juego Gato terminado, me falta entenderle, pero terminado

--- 6_JuegoGato/main.js
+++ 6_JuegoGato/main.js
@@ -25,10 +25,207 @@ function startGame(){
 }
 
 function playerPlays(){
+    const cells = document.querySelectorAll(".cell");
 
+    cells.forEach((cell, i) =>{//Itero desde el 0 al 8 todas las celdas
+        const column = i % 3;
+        const row = parseInt(i / 3);
+
+        if(board[row][column] === ""){//solo las celdas vacias pueden tener el evento click
+            cell.addEventListener("click", (e) =>{
+                board[row][column] = "O"; //Asignamos el valor a la matriz
+                //Tomamos el valor de la matriz y lo asignamos a la celda
+                cell.textContent = board[row][column]; 
+
+                turn = 1;
+                const won = checkIfWinner();
+
+                if(won === 'none'){
+                    PCPlays();//cedo el turno a la pc
+                    return;
+                }
+
+                if(won === 'draw'){
+                    renderDraw();
+                    cell.removeEventListener('click', this);
+                    return;
+                }
+                
+            });
+        }
+    });
 }
 
 function PCPlays(){
+    renderCurrentPlayer();//
+
+    setTimeout(() =>{
+        let played = false;
+        const options = checkIfCanWin();
+
+        if(options.length > 0){
+            const bestOption = options[0];
+
+            for(let i = 0; i < bestOption.length; i++){
+                if(bestOption[i].value === 0){
+                    const posi = bestOption[i].i;
+                    const posj = bestOption[i].j;
+
+                    board[posi][posj] = 'X';
+                    player = true;
+                    break;
+                }
+            }
+        }else{
+            for(let i = 0; i < board.length; i++){
+                for(let j = 0; j < board.length; j++){
+                    if(board[i][j] === "" && !played){
+                        board[i][j] = "X";
+                        played = true;
+                    }
+                }
+            }
+        }
+
+        turn = 0;
+        renderBoard();
+        renderCurrentPlayer();
+
+        const won = checkIfWinner();
+
+        if(won === 'none'){
+            playerPlays();
+            return;
+        }
+
+        if(won === "draw"){
+            playerPlays();
+            renderDraw();
+            return;
+        }
+        
+    }, 1500);
+}
+
+function renderDraw(){
+    playerDiv.textContent = "Draw";
+}
+
+function checkIfCanWin(){
+    const arr = JSON.parse(JSON.stringify(board));//Copiar la matriz
+
+    //Iteramos la copia de la matriz
+    for(let i = 0; i<arr.length; i++){
+        for(let j = 0; j<arr.length; j++){
+            //Si coincide con algún if, asignamos un objeto con 3 atributos
+            //a la posicion de la matriz copiada
+            if(arr[i][j] === "X"){
+                arr[i][j] = {value: 1, i, j};
+            }
+
+            if(arr[i][j] === ""){
+                arr[i][j] = {value: 0, i, j};
+            }
+
+            if(arr[i][j] === "O"){
+                arr[i][j] = {value: -2, i, j};
+            }
+
+        }
+    }
+
+    //Guardamos las posiciones como variables individuales
+    //Fila1
+    const p1 = arr[0][0];
+    const p2 = arr[0][1];
+    const p3 = arr[0][2];
+    //Fila 2
+    const p4 = arr[1][0];
+    const p5 = arr[1][1];
+    const p6 = arr[1][2];
+    //Fila 3
+    const p7 = arr[2][0];
+    const p8 = arr[2][1];
+    const p9 = arr[2][2];
+
+    //Combinaciones de posiciones posibles para ganar, en orden consecutivo
+    //Por filas
+    const s1 = [p1, p2, p3];
+    const s2 = [p4, p5, p6];
+    const s3 = [p7, p8, p9];
+    //Por columnas
+    const s4 = [p1, p4, p7];
+    const s5 = [p2, p5, p8];
+    const s6 = [p3, p6, p9];
+    //Por diagonales
+    const s7 = [p1, p5, p9];
+    const s8 = [p3, p5, p7];
+
+    const res = [s1, s2, s3, s4, s5, s6, s7, s8].filter((line) =>{
+        return (
+            line[0].value + line[1].value + line[2].value === 2 ||
+            line[0].value + line[1].value + line[2].value === -4
+        );
+    });
+
+    return res;
+}
+
+function checkIfWinner(){
+    //Guardamos las posiciones como variables individuales
+    //Fila1
+    const p1 = board[0][0];
+    const p2 = board[0][1];
+    const p3 = board[0][2];
+    //Fila 2
+    const p4 = board[1][0];
+    const p5 = board[1][1];
+    const p6 = board[1][2];
+    //Fila 3
+    const p7 = board[2][0];
+    const p8 = board[2][1];
+    const p9 = board[2][2];
+
+    //Combinaciones de posiciones posibles para ganar, en orden consecutivo
+    //Por filas
+    const s1 = [p1, p2, p3];
+    const s2 = [p4, p5, p6];
+    const s3 = [p7, p8, p9];
+    //Por columnas
+    const s4 = [p1, p4, p7];
+    const s5 = [p2, p5, p8];
+    const s6 = [p3, p6, p9];
+    //Por diagonales
+    const s7 = [p1, p5, p9];
+    const s8 = [p3, p5, p7];
+
+    const res = [s1, s2, s3, s4, s5, s6, s7, s8].filter((line) =>{
+        return (
+            line[0] + line[1] + line[2] === "XXX" ||
+            line[0] + line[1] + line[2] === "OOO" 
+        );
+    });
+
+    if(res.length > 0){
+        //Hay un ganador
+        if(res[0][0] === "X"){
+            playerDiv.textContent = "PC WINS";
+            return "pcwon";
+        }else{
+            playerDiv.textContent = "USER WINS";
+            return "userwon";
+        }
+    }else{
+        let draw = true;
+        for(let i = 0; i < board.length; i++){
+            for(let j = 0; j < board.length; j++){
+                if(board[i][j] === ""){
+                    draw = false;
+                }
+            }
+        }
+        return draw? 'draw' : 'none';
+    }
     
 }
 
